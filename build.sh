85d2c2a8a16e98527c90aa8b344149b9a5afc7b4
Dudecake <ckoomen@ckoomen.eu>
2023-10-31 21:28:04 +0100

    Added `RELEASEVER` as variable in `build.sh`

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ KEY=${KEY:-920498D5E1E4D38C258A1AE623FE6D6C9114BC76}
 DIST_NAME=iot
 DIST_PATH=${DIST_PATH:-/srv/http/ckoomen.eu/ostree/iot}
 CACHE_PATH=${CACHE_PATH:-/var/cache/rpm-ostree/centos/${DIST_NAME}}
+RELEASEVER="$(grep -Po '(?<=releasever: ")[^"]+' ${DIR}/centos-common-ostree.yaml)"
 MACHINE="$(uname -m)"
 
 set -e
@@ -24,18 +25,18 @@ if [[ ! -d "${DIST_PATH}/tmp" ]]; then
   mkdir -p ${DIST_PATH}
   ostree init --repo="${DIST_PATH}" --mode=archive
 fi
-if [[ -f "${DIST_PATH}/refs/heads/centos/8/${MACHINE}/${DIST_NAME}" ]]; then
+if [[ -f "${DIST_PATH}/refs/heads/centos/${RELEASEVER}/${MACHINE}/${DIST_NAME}" ]]; then
   NEW_REPO=1
 fi
 cd ${DIR}
 rpm-ostree compose tree --repo="${DIST_PATH}" --cachedir="${CACHE_PATH}" "${DIR}/centos-iot.yaml" --unified-core
 if [[ -z "${NEW_REPO}" ]]; then
   ostree --repo="${DIST_PATH}" prune --only-branch fedora/${FEDORA_VERSION}/${MACHINE}/${DIST_NAME} --depth=5
-  ostree --repo="${DIST_PATH}" static-delta generate centos/8/${MACHINE}/${DIST_NAME}
+  ostree --repo="${DIST_PATH}" static-delta generate centos/${RELEASEVER}/${MACHINE}/${DIST_NAME}
 fi
-#ostree --repo="${DIST_PATH}" gpg-sign centos/8/${MACHINE}/${DIST_NAME} ${KEY}
+#ostree --repo="${DIST_PATH}" gpg-sign centos/${RELEASEVER}/${MACHINE}/${DIST_NAME} ${KEY}
 ostree --repo="${DIST_PATH}" summary -u
 # Deploy with
 # ostree remote add centos-iot http://${URL}/ostree/iot
-# ostree pull centos-iot:centos/8/${MACHINE}/${DIST_NAME}
-# ostree admin deploy --os=centos-iot centos-iot:centos/8/${MACHINE}/${DIST_NAME}
+# ostree pull centos-iot:centos/${RELEASEVER}/${MACHINE}/${DIST_NAME}
+# ostree admin deploy --os=centos-iot centos-iot:centos/${RELEASEVER}/${MACHINE}/${DIST_NAME}
