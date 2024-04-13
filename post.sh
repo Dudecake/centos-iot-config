9bdc75437b041ee32c633c5b439ef8ad580fe755
Dudecake <ckoomen@ckoomen.eu>
2024-04-13 09:20:04 +0200

    Removed `journald` workaround

--- post.sh
+++ post.sh
@@ -5,13 +5,6 @@
 
 set -xeuo pipefail
 
-# Work around https://bugzilla.redhat.com/show_bug.cgi?id=1265295
-# Also note the create-new-then-rename dance for rofiles-fuse compat
-if ! grep -q '^Storage=persistent' /etc/systemd/journald.conf; then
-    (cat /etc/systemd/journald.conf && echo 'Storage=persistent') > /etc/systemd.journald.conf.new
-    mv /etc/systemd.journald.conf{.new,}
-fi
-
 # See: https://src.fedoraproject.org/rpms/glibc/pull-request/4
 # Basically that program handles deleting old shared library directories
 # mid-transaction, which never applies to rpm-ostree. This is structured as a
